840e859f74c315ba5fad3888f456fdcbe48b9d9b
Mikkel Jeppesen <6906805-Duckle29@users.noreply.gitlab.com>
2023-07-16 12:14:56 +0200

    Added mosquitto MQTT wss/mqtts setup

---
+++ filter_plugins/filters.py
@@ -0,0 +1,29 @@
+# Custom filters for use with ansible
+
+# https://stackoverflow.com/a/74247083
+def mosquitto_passwd(passwd):
+
+    from hashlib import pbkdf2_hmac
+    from base64 import b64encode
+    import secrets
+
+    # See https://github.com/eclipse/mosquitto/blob/master/src/password_mosq.h
+    iterations = 101
+
+    salt = secrets.token_bytes(12)
+    dk = pbkdf2_hmac('sha512',
+                     bytes(passwd, 'utf-8'),
+                     salt,
+                     iterations
+                     )
+    return (
+            "$7$" + str(iterations) + "$" +
+            b64encode(salt).decode() + "$" +
+            b64encode(dk).decode()
+            )
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'mosquitto_passwd': mosquitto_passwd,
+        }
